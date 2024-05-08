126ffbc9b6b57882bdf561a601738596cc4c880d
Niklas Dießner <40034835+TheBlackSheep3@users.noreply.github.com>
2024-05-08 10:39:31 +0200

    optionally pass expression as parameter

--- main.cpp
+++ main.cpp
@@ -1,16 +1,27 @@
 #include <any>
+#include <cstddef>
 #include <iostream>
 #include <string>
+#include <sstream>
 
 #include "antlr4-runtime.h"
 #include "ExpressionLexer.h"
 #include "ExpressionParser.h"
 #include "EvaluationVisitor.h"
 
-int main(int argc, char const* agrv[])
+int main(int argc, char const* argv[])
 {
   std::string input;
-  std::getline(std::cin, input);
+  if (1 < argc) {
+    std::stringstream argumentStream;
+    for (size_t index = 1; index < argc; ++index) {
+      argumentStream << argv[index];
+    }
+    input = argumentStream.str();
+  }
+  else {
+    std::getline(std::cin, input);
+  }
   antlr4::ANTLRInputStream stream(input);
   ExpressionLexer lexer(&stream);
   antlr4::CommonTokenStream tokens(&lexer);
